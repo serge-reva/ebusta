ca2366114b06c5788dfa736cac009556b6b66d94
Serge Reva <list.reva@gmail.com>
2026-01-26 22:11:03 +0300

    fix: улучшена токенизация кавычек и добавлен жадный захват в ядро парсера

--- lisp-converter/dsl-service.lisp
+++ lisp-converter/dsl-service.lisp
@@ -14,7 +14,7 @@
 
 (defvar *verbose* nil)
 
-;; --- 1. ЛЕКСИКА ---
+;;; --- 1. ЛЕКСИКА (UR 2.1 Fixed) ---
 
 (defun get-priority (op)
   (cond ((string-equal op "NOT") 3)
@@ -23,13 +23,13 @@
         (t 0)))
 
 (defun tokenize (str)
-  "Токенизатор с поддержкой закавыченных строк как единых блоков."
+  "Четко отделяет префикс поля (word:) от значения."
   (re:all-matches-as-strings "(\"[^\"]+\"|[a-zA-Z0-9_]+:|AND|OR|NOT|\\(|\\)|/|\\S+)" str))
 
 (defun numeric-p (s)
   (if (re:scan "^[0-9]+$" s) t nil))
 
-;; --- 2. ОБРАБОТКА ПОЛЕЙ (ЖАДНЫЙ ЗАХВАТ) ---
+;;; --- 2. ОБРАБОТКА ПОЛЕЙ (Greedy & Quoted) ---
 
 (defun make-field-node (field val-raw)
   (let* ((val (string-trim " \"" val-raw))
@@ -37,30 +37,25 @@
     `(:field ,field ,(if is-regex (string-trim "/" val) val) ,@(when is-regex '(:op :regex)))))
 
 (defun process-field (token rest-tokens-var)
-  "Реализация жадного захвата согласно требованиям:
-   1. Если значение в кавычках - берем только его.
-   2. Если нет - берем все токены до первого оператора или скобки."
-  (let* ((pos (position #\: token))
-         (field (subseq token 0 pos))
-         (val-part (subseq token (1+ pos)))
+  "Реализует логику: если после ':' идет кавычка - берем один токен, иначе - жадно до оператора."
+  (let* ((field (string-right-trim ":" token))
          (first-next (car rest-tokens-var)))
     (cond 
-      ;; Случай field:"value"
-      ((string/= "" val-part)
-       (values (make-field-node field val-part) rest-tokens-var))
-      ;; Случай field: "quoted value"
+      ;; Кавычки: author: "Стивен Кинг"
       ((and first-next (re:scan "^\".*\"$" first-next))
        (values (make-field-node field (pop rest-tokens-var)) rest-tokens-var))
-      ;; Жадный захват: field: Стивен Кинг AND ... -> заберет 'Стивен Кинг'
+      ;; Жадный захват: author: Стивен Кинг AND ...
       (t (let (collected)
            (loop while (and (car rest-tokens-var)
                             (zerop (get-priority (car rest-tokens-var)))
                             (not (member (car rest-tokens-var) '("(" ")") :test #'string-equal)))
                  do (push (pop rest-tokens-var) collected))
-           (values (make-field-node field (format nil "~{~A~^ ~}" (nreverse collected)))
+           (values (make-field-node field (if collected 
+                                            (format nil "~{~A~^ ~}" (nreverse collected))
+                                            ""))
                    rest-tokens-var))))))
 
-;; --- 3. CORE ENGINE ---
+;;; --- 3. SHUNTING-YARD ENGINE ---
 
 (defun parse-raw-to-sexp (str)
   (let ((token-list (tokenize str))
@@ -71,8 +66,7 @@
         (cond
           ((> (get-priority token) 0)
            (loop while (and stack 
-                            (> (get-priority (car stack)) 0)
-                            (>= (get-priority (car stack)) (get-priority token)))
+                            (> (get-priority (car stack)) (get-priority token)))
                  do (push (pop stack) output))
            (push token stack))
           ((string= token "(") (push token stack))
@@ -87,22 +81,19 @@
           (t (let ((field (if (numeric-p token) "id" "any")))
                (push (make-field-node field token) output))))))
     (loop while stack do (push (pop stack) output))
-    (build-tree-from-rpn (nreverse output))))
+    (let (stack-eval)
+      (dolist (token (nreverse output) (car stack-eval))
+        (if (and (listp token) (eq (car token) :field))
+            (push token stack-eval)
+            (let ((op (string-upcase (string token))))
+              (cond 
+                ((string= op "NOT") (push `(:not ,(pop stack-eval)) stack-eval))
+                ((member op '("AND" "OR") :test #'string=)
+                 (let* ((right (pop stack-eval)) (left (pop stack-eval))
+                        (key (if (string= op "AND") :and :or)))
+                   (push `(,key ,left ,right) stack-eval))))))))))
 
-(defun build-tree-from-rpn (rpn)
-  (let (stack)
-    (dolist (token rpn (car stack))
-      (if (and (listp token) (eq (car token) :field))
-          (push token stack)
-          (let ((op (string-upcase (string token))))
-            (cond 
-              ((string= op "NOT") (push `(:not ,(pop stack)) stack))
-              ((member op '("AND" "OR") :test #'string=)
-               (let* ((right (pop stack)) (left (pop stack))
-                      (key (if (string= op "AND") :and :or)))
-                 (push `(,key ,left ,right) stack)))))))))
-
-;; --- 4. AST & gRPC (БЕЗ ИЗМЕНЕНИЙ) ---
+;;; --- 4. AST & gRPC (ИНФРАСТРУКТУРА) ---
 
 (defun parse-sexp-to-ast (sexp &key request-id canonical-form)
   (let ((query (pb:make-search-query)))
@@ -132,33 +123,18 @@
          (request-id (format nil "req-~A" (get-universal-time))))
     (handler-case
         (let* ((sexp (parse-raw-to-sexp raw))
-               (ast (parse-sexp-to-ast sexp 
-                                      :request-id request-id 
-                                      :canonical-form (format nil "~S" sexp))))
-          (when *verbose*
-            (format t "[~A] Raw: ~A~%[~A] S-Exp: ~S~%" request-id raw request-id sexp)
-            (finish-output))
+               (ast (parse-sexp-to-ast sexp :request-id request-id :canonical-form (format nil "~S" sexp))))
+          (when *verbose* (format t "[~A] S-Exp: ~S~%" request-id sexp) (finish-output))
           ast)
-      (error (e)
-        (format t "[ERR ~A] ~A~%" request-id e)
-        (finish-output)
-        (pb:make-search-query :request-id request-id)))))
+      (error (e) (pb:make-search-query :request-id request-id)))))
 
 (defun start (&key (port 50052) (workers 8) (verbose nil))
   (setf *verbose* verbose)
   (grpc:init-grpc)
-  (format t "=== EBusta DSL Engine V21 [Port ~A, Workers ~A, Verbose ~A] ===~%" port workers *verbose*)
-  (finish-output)
+  (format t "=== EBusta DSL Engine V22 [Port ~A] ===~%" port) (finish-output)
   (grpc:run-grpc-proto-server (format nil "0.0.0.0:~A" port) 'pb:message-converter)
   (loop (sleep 1)))
 
 (defun build-binary ()
-  #+sbcl
-  (sb-ext:save-lisp-and-die "dsl-converter"
-                            :executable t
-                            :toplevel (lambda ()
-                                        (let ((args sb-ext:*posix-argv*))
-                                          (start :port 50052 
-                                                 :workers 8
-                                                 :verbose (or (member "-v" args :test #'string=)
-                                                              (member "--verbose" args :test #'string=)))))))
+  #+sbcl (sb-ext:save-lisp-and-die "dsl-converter" :executable t 
+                                  :toplevel (lambda () (start :port 50052 :verbose t))))
